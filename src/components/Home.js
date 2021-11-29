c3042eeb06817cc4aaa81dac5849c46db33d4432
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-11-29 19:59:28 +0100

    first version + header added

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Home=() =>{
+    return (
+<div class="wrap-body">
+   <div class="top">
+	<div class="zerogrid">
+		<div class="row">
+			<div class="f-left">
+				<span><i class="fa fa-map-marker"></i> RBK Tunisie C-16 </span>
+				<span><i class="fa fa-phone"></i> +216 52 779 617</span>
+				<span><i class="fa fa-envelope"></i> Elyesbenkhoud377@gmail.com</span>
+				<span> <i class="fas fa-futbol"> </i>Takwira Louled</span>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
+    )
+}
+
+export default Home
