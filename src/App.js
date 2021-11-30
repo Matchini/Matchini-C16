7c0052b076ae68231ca4757445b0e327ec6bfa5c
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-11-30 18:25:36 +0100

    login added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Home from "./components/Home";
+import Login from "./components/Login";
 import Nav from "./components/Nav";
 class App extends Component {
   render() {
     return (
       <div>
         <Home />
-        <Nav />
+        {/* <Nav /> */}
+        <Login />
       </div>
     );
   }
