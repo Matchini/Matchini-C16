33056c203aa1be307891613aa32bcc53b2fa79b9
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-03 22:38:12 +0100

    footer added but will be used in the landing page not in login or register

--- src/App.js
+++ src/App.js
@@ -5,22 +5,28 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 // import Nav from "./components/Nav";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Footer from "./components/Footer";
 class App extends Component {
   render() {
     return (
-      <div className="grid grid-cols-4 flex ">
-        <div className="col-span-4">
-          <Home />
-          {/* <Nav /> */}
-        </div>
-        <div className="col-start-1 col-end-3 ml-12 mr-12">
-          <Register />
-        </div>
-        {/* <div className="col-start-1 col-end-3 h-max">
+      <div>
+        <div className="grid grid-cols-4 flex ">
+          <div className="col-span-4">
+            <Home />
+            {/* <Nav /> */}
+          </div>
+          <div className="col-start-1 col-end-3 ml-12 mr-12">
+            <Register />
+          </div>
+          {/* <div className="col-start-1 col-end-3 h-max">
             <img src="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX5894361.jpg"/>
           </div> */}
-        <div className="col-start-3 col-end-5 ml-12 mr-12">      
-          <Login />
+          <div className="col-start-3 col-end-5 ml-12 mr-12">
+            <Login />
+          </div>
+        </div>
+        <div>
+          <Footer />
         </div>
       </div>
     );
