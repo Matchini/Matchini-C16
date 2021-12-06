c012e27f1fb5afed9e0490d62fb5bf68756d9587
heniLtaief <heniltaief@gmail.com>
2021-12-06 14:26:29 +0100

    logo to landingpage fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Login from "./Login";
 import LandingPage from "./LandingPage";
-import { useHistory } from 'react-router-dom';
+
 
 const Home = () => {
-  const history= useHistory();
 
   return (
     <div className="wrap-body">
       <div className="top flex items-center">
-      <img onClick={() => history.push('/LandingPage')} className="h-12 w-20 ml-12" src="https://lh3.googleusercontent.com/proxy/qboTH2huBGtNTLBLo7QiMtGB37XP2cRAmxm7sB6t86pf53zGvDydsOUB3QAORfBXoJztCTb5fSaPci9snvUD8Us0WcYc-lGhCRFZxjiNLZXyUIkpUnTO2xZ5dkKrXYycUYUAu9_r-i_Iz_eeCxb-44ro1g"/>
+      <img onClick={event =>  window.location.href='/'} className="h-12 w-20 ml-12" src="https://lh3.googleusercontent.com/proxy/qboTH2huBGtNTLBLo7QiMtGB37XP2cRAmxm7sB6t86pf53zGvDydsOUB3QAORfBXoJztCTb5fSaPci9snvUD8Us0WcYc-lGhCRFZxjiNLZXyUIkpUnTO2xZ5dkKrXYycUYUAu9_r-i_Iz_eeCxb-44ro1g"/>
         <div className="zerogrid">
           <div className="row">
             <div className="f-left">
