0d31fbac229cf2f9cf66d8c93622dc5ca2f22fb3
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-07 10:17:40 +0100

    register with google fixed also

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import Login from "./Login";
 import { useHistory } from "react-router-dom";
 import Footer from "./Footer";
+import { GoogleLogin } from "react-google-login";
 
 const Register = () => {
   const { register, handleSubmit, reset } = useForm();
@@ -18,6 +19,12 @@ const Register = () => {
       .catch((err) => {
         console.log(err);
       });
+  };
+  const responseGoogleSuccess = () => {
+    history.push("/Commercial");
+  };
+  const responseGoogleFail = () => {
+    history.push("/");
   };
 
   const history = useHistory();
@@ -127,12 +134,13 @@ const Register = () => {
                   <p className="text-lg font-semibold">OR</p>
                 </div>
                 <div class="flex mt-2 space-x-2 justify-center bg-gray-700 hover:bg-gray-600 text-white py-2 rounded-md transition duration-100">
-                  <img
-                    class=" h-6 cursor-pointer mb-1"
-                    src="https://i.imgur.com/arC60SB.png"
-                    alt=""
+                  <GoogleLogin
+                    clientId="106433618456-ui38ga6ajm39d35punapvavkk46fsmjc.apps.googleusercontent.com"
+                    buttonText="Login with your google account"
+                    onSuccess={responseGoogleSuccess}
+                    onFailure={responseGoogleFail}
+                    cookiePolicy={"single_host_origin"}
                   />
-                  <button>sign-up with google</button>
                 </div>
               </form>
             </div>
