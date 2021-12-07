99b9b8eea5c3c884ec0cfe6b84d9a5b6b4e64e69
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-07 11:46:23 +0100

    sidebar deleted

--- src/components/Commercial.js
+++ src/components/Commercial.js
@@ -1,16 +1,10 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-import Sidebar from "./Sidebar";
-
 function Commercial() {
   const location = useLocation();
   useEffect(() => {}, [location]);
-  return (
-    <div>
-      <Sidebar />
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default Commercial;
