46d76ee28579f3ea9b4217eb7845af3966391e68
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-08 20:31:35 +0100

    after backend deoply ! & last version useEffect rendering after 5 s

--- src/components/Posts/CreatePost.js
+++ src/components/Posts/CreatePost.js
@@ -6,7 +6,7 @@ const CreatePost = () => {
   const { register, handleSubmit, reset } = useForm();
   const onSubmit = (data) => {
     axios
-      .post("http://localhost:3000/match", data)
+      .post("https://matchini.herokuapp.com/match", data)
       .then((res) => {
         console.log(res.data);
         reset();
