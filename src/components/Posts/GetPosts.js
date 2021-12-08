3c61d22144a394342dd13961444d8437d6970bab
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-08 11:41:13 +0100

    fetching the matches is working but not optimised performance

--- src/components/Posts/GetPosts.js
+++ src/components/Posts/GetPosts.js
@@ -2,31 +2,28 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 const GetPosts = () => {
   const [posts, setPosts] = useState([]);
-  const GetData = () => {
+
+  useEffect(() => {
     axios
       .get(`http://localhost:3000/matches/`)
       .then((res) => {
         setPosts(res.data);
-        console.log(posts);
       })
       .catch((err) => {
         console.log(err);
       });
-  };
-
-  useEffect(() => {
-    GetData();
-  }, []);
+  });
 
   return (
     <div>
-      {posts.map((e, key) => {
-        return (
-          <div key={key}>
-            <h1>{e.fullName}</h1>
-          </div>
-        );
-      })}
+      {posts &&
+        posts.map((e, key) => {
+          return (
+            <div key={key}>
+              <h1>{e.fullName}</h1>
+            </div>
+          );
+        })}
     </div>
   );
 };
