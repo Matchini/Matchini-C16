e08b7c94eb81868eb4d9a941e864f612328f1811
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-09 11:42:40 +0100

    footer modification

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,16 +9,15 @@ const Footer = () => {
         <div className="container max-w-7xl mx-auto px-4">
           <div className="flex flex-wrap text-center lg:text-left pt-6">
             <div className="w-full lg:w-6/12 px-4">
-              <H5 color="gray">Material Tailwind</H5>
+              <H5 color="gray">Matchini </H5>
               <div className="-mt-4">
                 <LeadText color="blueGray">
-                  Easy to use React components for Tailwind CSS and Material
-                  Design.
+                  Our conecpt is to help you find your teammate
                 </LeadText>
               </div>
               <div className="flex gap-2 mt-6 md:justify-start md:mb-0 mb-8 justify-center">
                 <a
-                  href="https://www.facebook.com/CreativeTim"
+                  href="https://www.facebook.com/"
                   className="grid place-items-center bg-white text-blue-600 shadow-md font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -26,7 +25,7 @@ const Footer = () => {
                   <Icon family="font-awesome" name="fab fa-facebook-square" />
                 </a>
                 <a
-                  href="https://www.twitter.com/creativetim"
+                  href="https://www.twitter.com/"
                   className="grid place-items-center bg-white text-blue-400 shadow-md font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -34,7 +33,7 @@ const Footer = () => {
                   <Icon family="font-awesome" name="fab fa-twitter" />
                 </a>
                 <a
-                  href="https://www.instagram.com/creativetimofficial/"
+                  href="https://www.instagram.com/"
                   className="grid place-items-center bg-white text-indigo-500 shadow-md font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -42,23 +41,16 @@ const Footer = () => {
                   <Icon family="font-awesome" name="fab fa-instagram" />
                 </a>
                 <a
-                  href="https://www.dribbble.com/creativetim"
+                  href="https://www.dribbble.com/"
                   className="grid place-items-center bg-white text-pink-400 shadow-md font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <Icon family="font-awesome" name="fab fa-dribbble" />
                 </a>
+
                 <a
-                  href="https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w"
-                  className="grid place-items-center bg-white text-red-600 shadow-md font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Icon family="font-awesome" name="fab fa-youtube" />
-                </a>
-                <a
-                  href="https://github.com/creativetimofficial"
+                  href="https://github.com/ElyesBenkhoud"
                   className="grid place-items-center bg-white text-gray-900 shadow-md font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none"
                   target="_blank"
                   rel="noopener noreferrer"
