f0614ed7e2a215f025471fd276f7df2d954f0862
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-09 11:28:34 +0100

    home page icon added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,34 @@
 import React from "react";
 const Home = () => {
-  // onClick={(event) => (window.location.href = "/")}
-
   return (
-    <div className="wrap-body">
+    <div className="wrap-body ">
       <div className="top flex items-center">
         <div className="zerogrid">
           <div className="row">
-            <div className="f-left">
+            <div className="f-right">
               <span className="text-white">
-                <i className="fa fa-map-marker"></i> RBK Tunisie C-16{" "}
+                <i className="fa fa-map-marker"></i> All Tunisia
               </span>
               <span className="text-white">
                 <i className="fa fa-phone"></i> +216 52 779 617
               </span>
               <span className="text-white">
-                <i className="fa fa-envelope"></i> Elyesbenkhoud377@gmail.com
+                <i className="fa fa-envelope"></i> Matchini@gmail.com
               </span>
               <span className="text-white">
                 {" "}
                 <i className="fas fa-futbol"> </i> Don't walk through life just
-                playing football.
+                playing football
               </span>
+            </div>
+            <div>
+              <div
+                className="text-white "
+                onClick={(event) => (window.location.href = "/")}
+              >
+                {" "}
+                <i className="fa fa-home"> </i> Home page
+              </div>
             </div>
           </div>
         </div>
