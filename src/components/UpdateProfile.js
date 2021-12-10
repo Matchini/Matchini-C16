dcba7a6475931c34af9e4b38ad716d41c6ec69a5
slimkhemiri <khemiri.medslim1@gmail.com>
2021-12-10 11:18:34 +0000

    nav after login

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import Nav from "./Nav";
 const UpdateProfile = () => {
-  
   const history = useHistory();
 
-
-
-return (
-    <div
-      class="min-h-screen flex flex-col items-center justify-center bg-gray-100"
-    >
-     
-      <div
-        class="
+  return (
+    <div>
+      <Nav />
+      <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <div
+          class="
           flex flex-col
           bg-white
           shadow-md
@@ -25,81 +22,40 @@ return (
           w-50
           max-w-md
         "
-      >
-         <div>
-        <svg
-          onClick={() => history.goBack()}
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-10 w-10"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
-          />
-        </svg>
-      </div>
-        <div class="font-medium self-center text-xl sm:text-3xl text-gray-800">
-          Update your profile
-        </div>
-        
+          <div>
+            <svg
+              onClick={() => history.goBack()}
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-10 w-10"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
+              />
+            </svg>
+          </div>
+          <div class="font-medium self-center text-xl sm:text-3xl text-gray-800">
+            Update your profile
+          </div>
 
-        <div class="mt-10">
-          <form action="#">
-            <div class="flex flex-col mb-5">
-              <label
-                for="email"
-                class="mb-1 text-xs tracking-wide text-gray-600"
-                >Name:</label
-              >
-              <div class="relative">
-                <div
-                  class="
-                    inline-flex
-                    items-center
-                    justify-center
-                    absolute
-                    left-0
-                    top-0
-                    h-full
-                    w-10
-                    text-gray-400
-                  "
+          <div class="mt-10">
+            <form action="#">
+              <div class="flex flex-col mb-5">
+                <label
+                  for="email"
+                  class="mb-1 text-xs tracking-wide text-gray-600"
                 >
-                  <i class="fas fa-user text-green-500"></i>
-                </div>
-
-                <input
-                  id="email"
-                  type="email"
-                  name="email"
-                  class="
-                    text-sm
-                    placeholder-gray-500
-                    pl-10
-                    pr-4
-                    rounded-2xl
-                    border border-gray-400
-                    w-full
-                    py-2
-                    focus:outline-none focus:border-green-400
-                  "
-                  placeholder="Enter your name"
-                />
-              </div>
-            </div>
-            <div class="flex flex-col mb-5">
-              <label
-                for="email"
-                class="mb-1 text-xs tracking-wide text-gray-600">Address:
+                  Name:
                 </label>
-              <div class="relative">
-                <div
-                  class="
+                <div class="relative">
+                  <div
+                    class="
                     inline-flex
                     items-center
                     justify-center
@@ -110,16 +66,15 @@ return (
                     w-10
                     text-gray-400
                   "
-                >
-                  <i class="fas fa-address-card text-green-500"></i> 
-            
-                </div>
+                  >
+                    <i class="fas fa-user text-green-500"></i>
+                  </div>
 
-                <input
-                  id="email"
-                  type="email"
-                  name="email"
-                  class="
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    class="
                     text-sm
                     placeholder-gray-500
                     pl-10
@@ -130,19 +85,20 @@ return (
                     py-2
                     focus:outline-none focus:border-green-400
                   "
-                  placeholder="Enter your address"
-                />
+                    placeholder="Enter your name"
+                  />
+                </div>
               </div>
-            </div>
-            <div class="flex flex-col mb-5">
-              <label
-                for="email"
-                class="mb-1 text-xs tracking-wide text-gray-600"
-                >Phone number:</label
-              >
-              <div class="relative">
-                <div
-                  class="
+              <div class="flex flex-col mb-5">
+                <label
+                  for="email"
+                  class="mb-1 text-xs tracking-wide text-gray-600"
+                >
+                  Address:
+                </label>
+                <div class="relative">
+                  <div
+                    class="
                     inline-flex
                     items-center
                     justify-center
@@ -153,15 +109,15 @@ return (
                     w-10
                     text-gray-400
                   "
-                >
-                  <i class="fas fa-phone text-green-500"></i>
-                </div>
+                  >
+                    <i class="fas fa-address-card text-green-500"></i>
+                  </div>
 
-                <input
-                  id="email"
-                  type="email"
-                  name="email"
-                  class="
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    class="
                     text-sm
                     placeholder-gray-500
                     pl-10
@@ -172,20 +128,20 @@ return (
                     py-2
                     focus:outline-none focus:border-green-400
                   "
-                  placeholder="Enter your phone number"
-                />
+                    placeholder="Enter your address"
+                  />
+                </div>
               </div>
-            </div>
-            <div class="flex flex-col mb-5">
-              <label
-                for="email"
-                class="mb-1 text-xs tracking-wide text-gray-600">
-                    E-Mail Address:
-                    </label>
-              
-              <div class="relative">
-                <div
-                  class="
+              <div class="flex flex-col mb-5">
+                <label
+                  for="email"
+                  class="mb-1 text-xs tracking-wide text-gray-600"
+                >
+                  Phone number:
+                </label>
+                <div class="relative">
+                  <div
+                    class="
                     inline-flex
                     items-center
                     justify-center
@@ -196,15 +152,15 @@ return (
                     w-10
                     text-gray-400
                   "
-                >
-                  <i class="fas fa-at text-green-500"></i>
-                </div>
+                  >
+                    <i class="fas fa-phone text-green-500"></i>
+                  </div>
 
-                <input
-                  id="email"
-                  type="email"
-                  name="email"
-                  class="
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    class="
                     text-sm
                     placeholder-gray-500
                     pl-10
@@ -215,19 +171,21 @@ return (
                     py-2
                     focus:outline-none focus:border-green-400
                   "
-                  placeholder="Enter your email"
-                />
+                    placeholder="Enter your phone number"
+                  />
+                </div>
               </div>
-            </div>
-            <div class="flex flex-col mb-6">
-              <label
-                for="password"
-                class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
-                >Password:</label
-              >
-              <div class="relative">
-                <div
-                  class="
+              <div class="flex flex-col mb-5">
+                <label
+                  for="email"
+                  class="mb-1 text-xs tracking-wide text-gray-600"
+                >
+                  E-Mail Address:
+                </label>
+
+                <div class="relative">
+                  <div
+                    class="
                     inline-flex
                     items-center
                     justify-center
@@ -238,17 +196,15 @@ return (
                     w-10
                     text-gray-400
                   "
-                >
-                  <span>
-                    <i class="fas fa-lock text-green-500"></i>
-                  </span>
-                </div>
+                  >
+                    <i class="fas fa-at text-green-500"></i>
+                  </div>
 
-                <input
-                  id="password"
-                  type="password"
-                  name="password"
-                  class="
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    class="
                     text-sm
                     placeholder-gray-500
                     pl-10
@@ -259,15 +215,60 @@ return (
                     py-2
                     focus:outline-none focus:border-green-400
                   "
-                  placeholder="Enter your password"
-                />
+                    placeholder="Enter your email"
+                  />
+                </div>
               </div>
-            </div>
+              <div class="flex flex-col mb-6">
+                <label
+                  for="password"
+                  class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+                >
+                  Password:
+                </label>
+                <div class="relative">
+                  <div
+                    class="
+                    inline-flex
+                    items-center
+                    justify-center
+                    absolute
+                    left-0
+                    top-0
+                    h-full
+                    w-10
+                    text-gray-400
+                  "
+                  >
+                    <span>
+                      <i class="fas fa-lock text-green-500"></i>
+                    </span>
+                  </div>
 
-            <div class="flex w-full">
-              <button
-                type="submit"
-                class="
+                  <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    class="
+                    text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    rounded-2xl
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-green-400
+                  "
+                    placeholder="Enter your password"
+                  />
+                </div>
+              </div>
+
+              <div class="flex w-full">
+                <button
+                  type="submit"
+                  class="
                   flex
                   mt-2
                   items-center
@@ -284,30 +285,28 @@ return (
                   duration-150
                   ease-in
                 "
-              >
-                <span class="mr-2 uppercase">Submit</span>
-                <span>
-                  <svg
-                    class="h-6 w-6"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                </span>
-              </button>
-            </div>
-          </form>
+                >
+                  <span class="mr-2 uppercase">Submit</span>
+                  <span>
+                    <svg
+                      class="h-6 w-6"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                  </span>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-     
     </div>
-)
+  );
 };
 export default UpdateProfile;
