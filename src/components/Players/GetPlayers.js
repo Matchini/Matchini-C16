e3a7142c288832ee7a7140f102fef8f4f0275e98
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-11 17:07:27 +0100

    another card added

--- src/components/Players/GetPlayers.js
+++ src/components/Players/GetPlayers.js
@@ -83,78 +83,62 @@ const GetPlayers = () => {
           // </div>
 
           /////////////////////////////////////////////////////////////////////////////////
-          <body class="antialiased bg-gray-200 font-sans">
-            <div class="max-w-6xl mx-auto">
-              <div class="flex items-center justify-center min-h-screen">
-                <div class="max-w-sm w-full sm:w-1/2 lg:w-1/3 py-6 px-3">
-                  <div class="bg-white shadow-xl rounded-lg overflow-hidden">
-                    <div class="bg-cover bg-center h-56 p-4">
-                      <div class="flex justify-end">
-                        <svg
-                          class="h-6 w-6 text-white fill-current"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 1 0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42 0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z"></path>
-                        </svg>
-                      </div>
-                    </div>
-                    <div class="p-4">
-                      <p class="uppercase tracking-wide text-sm font-bold text-gray-700">
-                        {e.fullName}
-                      </p>
-                      <p class="text-gray-700">
-                        {e.availableTime} && {e.city}
-                      </p>
-                    </div>
-                    <div class="flex p-4 border-t border-gray-300 text-gray-700">
-                      <div class="flex-1 inline-flex items-center">
-                        <svg
-                          class="h-6 w-6 text-gray-600 fill-current mr-3"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M0 16L3 5V1a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v4l3 11v5a1 1 0 0 1-1 1v2h-1v-2H2v2H1v-2a1 1 0 0 1-1-1v-5zM19 5h1V1H4v4h1V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1h2V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1zm0 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1V6h-2v2a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6H3.76L1.04 16h21.92L20.24 6H19zM1 17v4h22v-4H1zM6 4v4h4V4H6zm8 0v4h4V4h-4z"></path>
-                        </svg>
-                        <p>
-                          <span class="text-gray-900 font-bold">3</span>{" "}
-                          {e.role}
-                        </p>
-                      </div>
-                      <div class="flex-1 inline-flex items-center">
-                        <svg
-                          class="h-6 w-6 text-gray-600 fill-current mr-3"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M17.03 21H7.97a4 4 0 0 1-1.3-.22l-1.22 2.44-.9-.44 1.22-2.44a4 4 0 0 1-1.38-1.55L.5 11h7.56a4 4 0 0 1 1.78.42l2.32 1.16a4 4 0 0 0 1.78.42h9.56l-2.9 5.79a4 4 0 0 1-1.37 1.55l1.22 2.44-.9.44-1.22-2.44a4 4 0 0 1-1.3.22zM21 11h2.5a.5.5 0 1 1 0 1h-9.06a4.5 4.5 0 0 1-2-.48l-2.32-1.15A3.5 3.5 0 0 0 8.56 10H.5a.5.5 0 0 1 0-1h8.06c.7 0 1.38.16 2 .48l2.32 1.15a3.5 3.5 0 0 0 1.56.37H20V2a1 1 0 0 0-1.74-.67c.64.97.53 2.29-.32 3.14l-.35.36-3.54-3.54.35-.35a2.5 2.5 0 0 1 3.15-.32A2 2 0 0 1 21 2v9zm-5.48-9.65l2 2a1.5 1.5 0 0 0-2-2zm-10.23 17A3 3 0 0 0 7.97 20h9.06a3 3 0 0 0 2.68-1.66L21.88 14h-7.94a5 5 0 0 1-2.23-.53L9.4 12.32A3 3 0 0 0 8.06 12H2.12l3.17 6.34z"
-                          ></path>
-                        </svg>
-                        <p>
-                          <span class="text-gray-900 font-bold">2</span>{" "}
-                          Bathrooms
-                        </p>
-                      </div>
-                    </div>
-                    <div class="px-4 pt-3 pb-4 border-t border-gray-300 bg-gray-100">
-                      <div class="text-xs uppercase font-bold text-gray-600 tracking-wide">
-                        Realtor
-                      </div>
-                      <div class="flex items-center pt-2">
-                        <div class="bg-cover bg-center w-10 h-10 rounded-full mr-3"></div>
-                        <div>
-                          <p class="font-bold text-gray-900">Tiffany Heffner</p>
-                          <p class="text-sm text-gray-700">{e.number}</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+          <div class="w-screen h-screen flex justify-center items-center">
+            <div class="container mx-auto max-w-sm rounded-lg overflow-hidden shadow-lg my-2 bg-white">
+              <div class="relative z-10">
+                <img
+                  class="w-full"
+                  src="https://unsplash.com/photos/xNdldoBUECo/download?force=true&w=640"
+                  alt="Profile image"
+                />
+                <div class="text-center absolute w-full">
+                  <p class="text-white tracking-wide uppercase text-lg font-bold">
+                    John Doe
+                  </p>
+                  <p class="text-gray-400 text-sm">@johndoe</p>
+                </div>
+              </div>
+              <div class="relative flex justify-between items-center flex-row px-6 z-50 -mt-10">
+                <p class="flex items-center text-gray-400">
+                  <span class="inline-block w-3 h-3 bg-green-500 rounded-full mr-2"></span>
+                  online
+                </p>
+                <button class="p-4 bg-red-600 rounded-full hover:bg-red-500 focus:bg-red-700 transition ease-in duration-200 focus:outline-none">
+                  <svg
+                    viewBox="0 0 20 20"
+                    enable-background="new 0 0 20 20"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      fill="#FFFFFF"
+                      d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
+                                          C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
+                                          C15.952,9,16,9.447,16,10z"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div class="pt-6 pb-8 text-gray-600 text-center">
+                <p>Fullstack Developer </p>
+                <p class="text-sm">Coding from Planet Earth</p>
+              </div>
+
+              <div class="pb-10 uppercase text-center tracking-wide flex justify-around">
+                <div class="posts">
+                  <p class="text-gray-400 text-sm">Posts</p>
+                  <p class="text-lg font-semibold text-blue-300">76</p>
+                </div>
+                <div class="followers">
+                  <p class="text-gray-400 text-sm">Followers</p>
+                  <p class="text-lg font-semibold text-blue-300">964</p>
+                </div>
+                <div class="following">
+                  <p class="text-gray-400 text-sm">Following</p>
+                  <p class="text-lg font-semibold text-blue-300">34</p>
                 </div>
               </div>
             </div>
-          </body>
+          </div>
         );
       })}
     </div>
