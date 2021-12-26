b07dfd9ee355cbe012144271d590038085d13a57
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-26 19:23:20 +0100

    clean code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from "react";
-// import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
-// import Nav from "./components/Nav";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 import Commercial from "./components/Commercial";
-// import CreateGame from "./components/CardsChoice/CreateGame";
-// import CreatePlayer from "./components/CardsChoice/CreatePlayer";
 import CommercialSec from "./components/CommercialSec";
 import CommercialThird from "./components/CommercialThird";
-// 215341427022-qpsrq4730unqiq2v0e0eci2valevqr1b.apps.googleusercontent.com
+
 const App = () => {
   return (
     <div className="bg-gray-100">
       <Home />
-
       <Router>
         <Switch>
           <Route path="/Register" exact>
@@ -39,19 +34,11 @@ const App = () => {
           <Route path="/commercialthird" exact>
             <CommercialThird />
           </Route>
-          {/* <Route path="/creategame" exact>
-            <CreateGame />
-          </Route>
-          <Route path="/createplayer" exact>
-            <CreatePlayer />
-          </Route> */}
-
           <LandingPage />
           <Footer />
         </Switch>
       </Router>
     </div>
-    // </div>
   );
 };
 
